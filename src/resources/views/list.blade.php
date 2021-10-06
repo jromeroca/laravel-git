2467a5394b2d6d950b0b1298e704741a53886639
jromeroca <jesus.romero84@outlook.com>
2021-10-06 08:56:28 +0200

    Update list.blade.php

--- src/resources/views/list.blade.php
+++ src/resources/views/list.blade.php
@@ -38,7 +38,7 @@
 
     <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
         <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
-            <img src="{{ asset("vendor/jromero/laravel-git/logo.svg") }}" width="300">
+            <img src="{{ asset("vendor/jromero/laravel-git/logo_azul.png") }}" width="300">
         </div>
 
         <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
